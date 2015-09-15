c29dbc48db8666a956ae38b069ac9d0d49a2eca9
Zhhittta <zhanght1111@outlook.com>
2015-09-15 11:23:55 +0800

    演示
    123

--- Beta-1.0/src/ZWH.java
+++ Beta-1.0/src/ZWH.java
@@ -3,12 +3,10 @@ public class ZWH {
 
 	public static void main(String[] args) {
 		int a=100;
-
-
-
-		int b=100;
+    int b=100;
    System.out.println(a+b);
    String s="sss";
+   s="sssssss";
    System.out.println(s);
 
 	}
